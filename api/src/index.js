b56614a70e4dbbbb8cf265f516863a0636c66917
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-17 03:07:10 -0300

    refactor

--- api/src/index.js
+++ api/src/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const msg = "API Running";
 
-app.get("/", (req, res) => res.json(msg));
+app.get("/", (_, res) => res.json(msg));
 
 app.listen(process.env.PORT || 3333, () => console.log(msg));
 
-// WEBSITE_NODE_DEFAULT_VERSION
+// 
