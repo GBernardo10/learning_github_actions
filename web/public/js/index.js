957335793391090032eb3a25f30b4122ef7bf46c
GBernardo10 <g_bernardo10@hotmail.com>
2020-11-17 05:56:03 -0300

    chore: change url paramenter

--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,5 +1,5 @@
 function getData(...optios) {
-  fetch("http://localhost:3333/")
+  fetch("http://api-portaltech.eastus.cloudapp.azure.com:3333/")
     .then((res) => res.json().then(makeDOM))
     .catch(console.log);
 }
@@ -8,7 +8,7 @@ function makeDOM(args) {
   const body = "background-color: rgb(225 219 247)";
   const style = `color:rgb(62 45 45); font-size:x-large`;
   const root = document.getElementById("h1_heading");
-  
+
   document.body.style = body;
   root.style = style;
   root.innerHTML = args.msg || "OFF";
